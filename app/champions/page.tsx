bc4410e4d6bfe108da9151df7c07c1ae31b73ee4
Nick <nicolas.cinera@gmail.com>
2023-06-05 23:47:57 -0400

    no main class

--- app/champions/page.tsx
+++ app/champions/page.tsx
@@ -1,7 +1,7 @@
 export default function Champions() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-6xl font-bold">Champions</h1>
-    </main>
+    </div>
   );
 }
--- app/match/page.tsx
+++ app/match/page.tsx
@@ -1,7 +1,7 @@
 export default function Match() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-6xl font-bold">Match</h1>
-    </main>
+    </div>
   );
 }
