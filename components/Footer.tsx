a58c812939bbcdf44352686c37c77f77be022665
Nick <nicolas.cinera@gmail.com>
2023-06-30 18:58:37 -0400

    Skelly Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+function Footer() {
+  return (
+    <footer className="footer-container">
+      <div className="footer-content">
+        <div className="footer-left">
+          <h3>Footer</h3>
+          <p>Footer content</p>
+        </div>
+        <div className="footer-center">
+          <h3>Footer</h3>
+          <p>Footer content</p>
+        </div>
+        <div className="footer-right">
+          <h3>Footer</h3>
+          <p>Footer content</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
