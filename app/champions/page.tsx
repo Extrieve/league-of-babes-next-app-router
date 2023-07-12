f5d59d788aa0995956229cd4752b77b1a0b54f4b
Nick <nicolas.cinera@gmail.com>
2023-07-11 22:44:36 -0400

    All Champions Page

--- app/champions/page.tsx
+++ app/champions/page.tsx
@@ -1,7 +1,29 @@
-export default function Champions() {
+import { getAllChampions, getVersions } from '@/api/Service';
+import ChampionCard from '@/components/ChampionCard';
+import Champion from '@/models/iChampion';
+import React from 'react';
+
+interface ChampionsPageProps {
+    champions: Champion[];
+}
+
+const ChampionsPage: React.FC<ChampionsPageProps> = ({ champions }) => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl font-bold">Champions</h1>
+    <div className="flex flex-wrap justify-around">
+      {champions.map(champion => (
+        <ChampionCard key={champion.id} champion={champion} />
+      ))}
     </div>
   );
+};
+
+// This will be called server-side and the returned object will be passed as props to your component
+export async function getServerSideProps() {
+  const version = await getVersions();
+  const champions = await getAllChampions(version);
+
+  // Will be passed to the page component as props
+  return { props: { champions } };
 }
+
+export default ChampionsPage;
