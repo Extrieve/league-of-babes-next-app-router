1ff9acba55a2cb4a035f6e478e31194a338c94ca
Nick <nicolas.cinera@gmail.com>
2023-07-11 23:27:42 -0400

    All Champions Page

--- app/champions/page.tsx
+++ app/champions/page.tsx
@@ -1,29 +1,32 @@
-import { getAllChampions, getVersions } from '@/api/Service';
-import ChampionCard from '@/components/ChampionCard';
-import Champion from '@/models/iChampion';
-import React from 'react';
+'use client';
 
-interface ChampionsPageProps {
-    champions: Champion[];
-}
+import { getAllChampions, getVersions } from "@/api/Service";
+import ChampionCard from "@/components/ChampionCard";
+import Champion from "@/models/iChampion";
+import { useEffect, useState } from "react";
 
-const ChampionsPage: React.FC<ChampionsPageProps> = ({ champions }) => {
+
+const ChampionsPage = (props: any) => {
+  const [champions, setChampions] = useState<Champion[]>([] as Champion[]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const version = await getVersions();
+      const champions: Champion[] = await getAllChampions(version);
+      
+      setChampions(champions);
+    };
+    
+    fetchData();
+  }, []); // Empty dependency array means this effect runs once on component mount
+  
   return (
     <div className="flex flex-wrap justify-around">
-      {champions.map(champion => (
+      {champions && champions.map(champion => (
         <ChampionCard key={champion.id} champion={champion} />
       ))}
     </div>
   );
 };
-
-// This will be called server-side and the returned object will be passed as props to your component
-export async function getServerSideProps() {
-  const version = await getVersions();
-  const champions = await getAllChampions(version);
-
-  // Will be passed to the page component as props
-  return { props: { champions } };
-}
 
 export default ChampionsPage;
