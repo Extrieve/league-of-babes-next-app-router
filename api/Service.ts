3f14458167389699f1f1a33558ad6a817f2c6d74
Nick <nicolas.cinera@gmail.com>
2023-07-11 22:44:20 -0400

    Get Version

--- api/Service.ts
+++ api/Service.ts
@@ -2,6 +2,11 @@ import axios from 'axios';
 
 const external_api_base_url = 'https://ddragon.leagueoflegends.com/cdn/';
 
+export const getVersions = async () => {
+  const response = await axios.get("https://ddragon.leagueoflegends.com/api/versions.json").then((response) => {return response.data.data;});
+  return response[0] ?? "13.13.1";
+};
+
 export const getAllChampions = async (version: string) => {
   const request_url = `${external_api_base_url}${version}/data/en_US/champion.json`;
   const response = await axios.get(request_url).then((res) => {
