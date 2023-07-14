03c512276eaca1e07e2e565635a97751c1325406
Nick <nicolas.cinera@gmail.com>
2023-07-14 14:34:25 -0400

    Footer Refactor

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,24 +1,24 @@
 'use client';
 
-function Footer() {
+import { FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa';
+
+export default function Footer() {
   return (
-    <footer className="footer-container">
-      <div className="footer-content">
-        <div className="footer-left">
-          <h3>Footer</h3>
-          <p>Footer content</p>
-        </div>
-        <div className="footer-center">
-          <h3>Footer</h3>
-          <p>Footer content</p>
-        </div>
-        <div className="footer-right">
-          <h3>Footer</h3>
-          <p>Footer content</p>
-        </div>
+    <footer className="bg-blue-500 text-white p-6 mt-6 flex justify-between items-center rounded-t-lg">
+      <div className="text-lg">
+        &copy; {new Date().getFullYear()} League of Babes. All rights reserved.
+      </div>
+      <div className="space-x-4">
+        <a href="https://www.facebook.com/" target="_blank" rel="noreferrer" className="text-white hover:text-blue-300 transition-colors duration-200">
+          <FaFacebookF />
+        </a>
+        <a href="https://www.instagram.com/" target="_blank" rel="noreferrer" className="text-white hover:text-blue-300 transition-colors duration-200">
+          <FaInstagram />
+        </a>
+        <a href="https://www.twitter.com/" target="_blank" rel="noreferrer" className="text-white hover:text-blue-300 transition-colors duration-200">
+          <FaTwitter />
+        </a>
       </div>
     </footer>
   );
 }
-
-export default Footer;
