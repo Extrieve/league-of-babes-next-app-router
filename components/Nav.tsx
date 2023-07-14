124d48a69c2f430b34b008d29cdf457e353bd9dc
Nick <nicolas.cinera@gmail.com>
2023-07-14 14:34:07 -0400

    Nav Refactor

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,8 +4,8 @@ import NavItem from "./NavItem";
 
 export default function Nav() {
   return (
-    <nav className="felx-between w-full mb-16 pt-3">
-      <h1 className="text-white text-lg font-bold">Nav</h1>
+    <nav className="flex justify-between items-center w-full mb-16 pt-3 px-5 bg-white shadow-lg text-gray-700 rounded-lg">
+      <h1 className="text-blue-500 text-lg font-bold">League of Babes</h1>
       <ul className="flex space-x-4">
         <NavItem href="/" linkText="Home" />
         <NavItem href="/champions" linkText="Champions" />
