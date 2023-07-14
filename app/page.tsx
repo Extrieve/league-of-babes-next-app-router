a1897728d7dacc5ff7a711b1eee2a3f53632f41a
Nick <nicolas.cinera@gmail.com>
2023-07-14 14:33:57 -0400

    Landing Page Refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,24 @@
-const Home = () => {
+'use client';
+
+import Link from 'next/link';
+
+const HomePage = () => {
+
   return (
-    <div className="home">
-      <h1>
-        League of Babes
-      </h1>
-      <p className="home-description">
-        Find out whos your favorite Babe across Summoners Rift 😳
-      </p>
-      <div className='go'>
-        <a href="/match">
-          <button className="home-button">
-            Find your Babe
-          </button>
-        </a>
-        <a href="/champions">
-          <button className="home-button">
-            Explore Babes
-          </button>
-        </a>
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <div className="bg-white rounded-lg shadow-2xl p-10 text-center" style={{maxWidth: '500px'}}>
+        <h1 className="text-4xl font-bold mb-6 text-blue-500">Welcome to League of Babes!</h1>
+        <p className="text-xl mb-6 text-gray-700">Discover the cutest League of Legends champion that matches your preference.</p>
+
+        <Link href="/champions" style={{ 
+          background: 'linear-gradient(to right, #ff9966, #ff5e62)', 
+          boxShadow: '0 4px 15px 0 rgba(255, 153, 102, 0.4), 0 4px 15px 0 rgba(255, 94, 98, 0.4)' 
+        }}>
+            Start Journey
+        </Link>
       </div>
     </div>
   );
 };
 
-export default Home;
+export default HomePage;
